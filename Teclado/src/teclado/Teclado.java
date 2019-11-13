6210fb5d1c1e164bec9ddf05ed19abe43a6062d4
Sofia-jimenez <53840229+Sofia-jimenez@users.noreply.github.com>
2019-11-12 19:21:09 -0600

    Add files via upload

---
+++ Teclado/src/teclado/Teclado.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teclado;
+
+/**
+ *
+ * @author fofib
+ */
+public class Teclado {
+    public static void main(String[] args) {
+        JFrameTeclado tecla = new JFrameTeclado();
+        tecla.setVisible(true);
+        tecla.setSize(800, 400);
+    }
+}
